c063c7bf9794c8f58d685e9b59ee15425c088c62
Y. Emre Ciftci <ciftciyunusemre20@gmail.com>
2022-10-09 20:05:01 +0300

    11 ve 12. Test cozumleri

--- src/Test_11.java
+++ src/Test_11.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class Test_11 {
     public static void main(String[] args) {
 
         // Klavyeden girilen 15 sayıdan pozitif sayıların
         // ortalamasını bulup ve negatif sayıların ortalamasını
         // bularak ekrana yazan uygulamasını yazınız.
+
+        Scanner sc=new Scanner(System.in);
+
+        int sayac=1;
+        int negatifSayac=0;
+        int negatifToplam=0;
+        int pozitifSayac=0;
+        int pozitifToplam=0;
+
+
+        while(sayac<=15){
+
+            System.out.print(sayac+". sayiyi girin: ");
+            int sayi=sc.nextInt();
+
+            if (sayi<0){
+                negatifToplam+=sayi;
+                negatifSayac++;
+            }else {
+                pozitifToplam+=sayi;
+                pozitifSayac++;
+            }
+            sayac++;
+        }
+
+        System.out.println(pozitifSayac+" adet pozitif girilen sayilarin ortalamasi: "+(pozitifToplam/pozitifSayac));
+        System.out.println(negatifSayac+" adet negatif girilen sayilarin ortalamasi: "+(negatifToplam/negatifSayac));
+
+
+
+
+
     }
 }
--- src/Test_12.java
+++ src/Test_12.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Test_12 {
     public static void main(String[] args) {
 
-        // ) Klavyeden taban ve üs değeri girilmek şartıyla
+        // Klavyeden taban ve üs değeri girilmek şartıyla
         // bir sayının kuvvetini hesaplayan programını yapınız.
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Taban sayisini girin: ");
+        int taban=sc.nextInt();
+        System.out.print("Us sayisini girin: ");
+        int us=sc.nextInt();
+
+        int sayac=1;
+        int ussu=1;
+
+        while (sayac<=us){
+            ussu*=taban;
+            sayac++;
+        }
+
+        System.out.println(taban+" sayisinin "+us+" ussu: "+ussu);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
