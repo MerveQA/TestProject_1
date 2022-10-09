d5fa36d76ca0b63cf80276889c9643de673c514b
M.Merve KOŞAR <merveabd.61@gmail.com>
2022-10-09 14:51:55 +0300

    completed

--- src/Test_16.java
+++ src/Test_16.java
@@ -3,5 +3,11 @@ public class Test_16 {
 
         //1-15 arasındaki sayıların küpleri
         // toplamını bularak ekrana yazdıran uygulamasını yazınız.
+
+        int toplam =0;
+        for(int i=2 ; i<15; i++){
+            toplam+=i*i*i;
+        }
+        System.out.println(toplam);
     }
 }
