c930c9cff5e6a2e587796d7c5ce19da45311acbb
Sena Özdemir Çelebi <senaozdemircelebi@gmail.com>
2022-10-11 09:17:48 +0300

    task

--- src/Test_02.java
+++ src/Test_02.java
@@ -8,5 +8,41 @@ public class Test_02 {
 
 
 
+        System.out.println("Python öğrenilmesi kolay bir programlama dilidir. ");
+
+        String metin = "Python öğrenilmesi kolay bir programlama dilidir. ";
+
+        String tersstr = "";
+
+        int metinuzunluk = metin.length() - 1;
+        for (int i = metinuzunluk; i >= 0; i--) {
+            tersstr += metin.charAt(i);
+        }
+        System.out.println("girilen ifadenin tersi : " + tersstr);
+        for (int i = 0; i <= metinuzunluk; i++) {
+            System.out.println((metin.charAt(i)));
+
+        }
+        String harf = "aeıioöuü";
+        int sayac = 0;
+        String sesli = String.valueOf('a');
+        for (int i = 0; i <= metinuzunluk; i++) {
+            for (int j = 0; j <= sesli.length(); j++) {
+
+
+                if (metin.charAt(i) == harf.charAt(j)) {
+                    sayac++;
+                }
+            }
+
+
+
+
+        }
+
+        System.out.print(sayac);
+
+
+
     }
 }
