27d329ed93a54fd067348af7c920929c4d30ffba
Ayse Sen <aysesen110211@gmail.com>
2022-10-13 13:32:41 +0300

    13

--- src/Test_14.java
+++ src/Test_14.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Test_14 {
     public static void main(String[] args) {
 
         // Rastgele tutulan 1 ile 150 arasında 50 adet sayının
         // 100’den büyük olan
         // sayıları bularak ekrana yazdıran uygulamasını yazınız.
+
+        Scanner s=new Scanner(System.in);
+
     }
+
 }
