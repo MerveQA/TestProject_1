a5a79aeceaa3fcb3ac9b637345eefb9920ef6502
Nazlı <ahm.2012.123@gmail.com>
2022-10-13 16:45:43 +0300

    13 ekim 2022

--- src/Test_08.java
+++ src/Test_08.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Test_08 {
     public static void main(String[] args) {
-        System.out.println("selam");
+        System.out.println("toplam almakta sorun var Nerede eksik var bakar msnz");
         // Klavyeden girilen 10 sayıdan
         // pozitif sayıların toplamını bulup ekrana yazdıran
         // uygulamasını yazınız.
+
+        Scanner scan = new Scanner(System.in);
+        int sayiArr []  = new int [11];
+        int toplam=0;
+        for (int i = 1; i < sayiArr.length; i++) {
+            System.out.print(i+"."+"Klavyeden bir sayı giriniz : ");
+            sayiArr[i] = scan.nextInt();
+            toplam+=sayiArr[i];
+
+
+
+            }
+
+
+        }
+
+
     }
-}
+
